9bcdecec2deb2a53c40fdda50ee983ff65ab2d53
STORMTR00P3R <noorblum16@gmail.com>
2024-04-13 15:39:05 -0700

    push

---
+++ app.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+
+set :database, { adapter: 'sqlite3', database: 'tweets.db' }
+
+# Tweet Model
+class Tweet < ActiveRecord::Base
+    # Models are used to define the structure and behavior of db tables
+    # Define associations, validations, and other model logic here
+end
