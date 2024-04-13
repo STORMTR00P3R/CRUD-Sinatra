8dbd0f2aa6d390c1ddc66827a438c13a278dc1b9
STORMTR00P3R <noorblum16@gmail.com>
2024-04-13 15:42:31 -0700

    hallo

--- app.rb
+++ app.rb
@@ -8,3 +8,7 @@ class Tweet < ActiveRecord::Base
     # Models are used to define the structure and behavior of db tables
     # Define associations, validations, and other model logic here
 end
+
+get '/' do
+    "Hello World"
+end
